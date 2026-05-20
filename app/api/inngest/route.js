64a8dd7c932cc39949d764d79378a3c069147fc8
Jaykesh Kumar <jaykeshkumar549@gmail.com>
2026-05-20 21:24:33 +0530

    Initial project setup

---
+++ app/api/inngest/route.js
@@ -0,0 +1,9 @@
+import { serve } from "inngest/next";
+import { inngest } from "../../../inngest/client";
+import { syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/inngest/functions";
+
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
+});
